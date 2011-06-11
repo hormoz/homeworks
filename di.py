dff3785f7d7cc5675f20d998dc5825006820fed0
Hormoz Z. Syakal <hormoz.x@gmail.com>
2011-06-11 09:36:13 +0200

    comment

--- di.py
+++ di.py
@@ -1,10 +1,14 @@
-#50 = 6(5) + 9(0) + 20(1)
-#51 = 6(7) + 9(1) + 20(0)
-#52 = 6(2) + 9(0) + 20(2)
-#53 = 6(4) + 9(1) + 20(1)
-#54 = 6(0) + 9(6) + 20(0)
-                                               #---- 6*a + 9*b + 20*c = num ----#
+# PROBLEM:
+#
+# Show that it is possible to buy exactly 50, 51, 52, 53, 54, and 55 McNuggets, by finding
+# solutions to the Diophantine equation. You can solve this in your head, using paper and pencil,
+# or writing a program. However you chose to solve this problem, list the combinations of 6, 9
+# and 20 packs of McNuggets you need to buy in order to get each of the exact amounts.
+# Given that it is possible to buy sets of 50, 51, 52, 53, 54 or 55 McNuggets by combinations of 6,
+# 9 and 20 packs, show that it is possible to buy 56, 57,..., 65 McNuggets. In other words, show
+# how, given solutions for 50-55, one can derive solutions for 56-65.
 
+#---- 6*a + 9*b + 20*c = num ----#
 
 #step 1: add 1 to C while C is not equal to Z & equation is not solved;
         #if  C=Z goto step 2
@@ -15,31 +19,48 @@
 #step 3: add 1 to A while A is not equal to Z & equation is not solved; set C and B to 0; go to step 1
         #if  A=Z -> No Possible Answer
 
-num = 180
-z = num/6
+num = input("enter a number: ")
+num = int(num)
 a, b, c = 0, 0, 0
 
-
+# a=0 b=0 c=counting
 while c < num:
     c += 1
-    if 9*b + 20*c == num: break
+    if 6*a + 9*b + 20*c == num: break
 
-if 9*b + 20*c != num: c = 0
+if 6*a + 9*b + 20*c != num: c = 0 # reseting value of c to 0
 
-
-while (b < num) and (9*b + 20*c != num):
+# a=0 b=counting c=counting
+while (b < num) and (6*a + 9*b + 20*c != num):
     b += 1
-    if (9*b + 20*c == num): break
+    if (6*a + 9*b + 20*c == num): break
     else:
-        while (c < num) and (9*b + 20*c != num):
+        while (c < num) and (6*a + 9*b + 20*c != num):
             c += 1
-        if 9*b + 20*c != num: c=0
+        if 6*a + 9*b + 20*c != num: c=0
+
+if 6*a + 9*b + 20*c != num: b, c = 0, 0 # reseting value of b & c to 0
+
+# a=counting b=counting c=counting
+while (a < num) and (6*a + 9*b + 20*c != num):
+    a += 1
+    if 6*a + 9*b + 20*c == num: break
+    else:
+        while (b < num) and (6*a + 9*b + 20*c != num):
+            b += 1
+            if (6*a + 9*b + 20*c == num): break
+            else:
+                while (c < num) and (6*a + 9*b + 20*c != num):
+                    c += 1
+            if 6*a + 9*b + 20*c != num: c=0
+    if 6*a + 9*b + 20*c != num: b=0
 
 
 
 
-
-
-print num, ("=")
+print (), num
 print ("c| 20 *"), c
 print ("b| 9 *"), b
+print ("a| 6 *"), a
+
+# KNOWN BUG: put 52, feelin' sleepy right now.
--- prime_numbers.py
+++ prime_numbers.py
@@ -2,7 +2,7 @@
 # Author: Hormoz Z. Syakal
 # Description: A code which prints out prime numbers up to 1000
 # Algorithm:
-# the way it works ia basicly tries to divide x to y; if the remender is not 0
+# it basicly tries to divide x to y; if the remender is not 0
 # it adds up 1 to the y until it is equal to x itself. x will be printed only
 # if y is equal to it.
 #
@@ -13,6 +13,6 @@ x=5
 while x < 1000:
     y=3
     while (x%y != 0) and (y <= x):
-        y += 1
+        y += 2
         if x == y: print x
     x += 2
